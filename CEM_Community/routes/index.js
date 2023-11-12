7a61827c9849de120edbbe9c27a575f38f04af37
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2023-11-12 22:53:24 +0900

    update router

--- CEM_Community/routes/index.js
+++ CEM_Community/routes/index.js
@@ -6,9 +6,19 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* board 페이지 라우팅 */
+router.get('/board', function(req, res, next) {
+  res.render('board'); // write.ejs 템플릿을 렌더링
+});
+
 /* write 페이지 라우팅 */
 router.get('/write', function(req, res, next) {
   res.render('write'); // write.ejs 템플릿을 렌더링
+});
+
+/* modify 페이지 라우팅 */
+router.get('/modify', function(req, res, next) {
+  res.render('modify'); // write.ejs 템플릿을 렌더링
 });
 
 module.exports = router;
