bdc1f8246dfd1b762844a7e02b78b1015a25e25b
PSLeon24 <59058869+PSLeon24@users.noreply.github.com>
2023-11-19 11:15:07 +0900

    Update index.js

--- CEM_Community/routes/index.js
+++ CEM_Community/routes/index.js
@@ -13,7 +13,7 @@ router.get('/main', function(req, res, next) {
 
 /* board 페이지 라우팅 */
 router.get('/board', function(req, res, next) {
-  res.render('board'); // write.ejs 템플릿을 렌더링
+  res.render('board'); // board.ejs 템플릿을 렌더링
 });
 
 /* write 페이지 라우팅 */
@@ -23,12 +23,12 @@ router.get('/write', function(req, res, next) {
 
 /* modify 페이지 라우팅 */
 router.get('/modify', function(req, res, next) {
-  res.render('modify'); // write.ejs 템플릿을 렌더링
+  res.render('modify'); // modify.ejs 템플릿을 렌더링
 });
 
 /* read 페이지 라우팅 */
 router.get('/read', function(req, res, next) {
-  res.render('read'); // write.ejs 템플릿을 렌더링
+  res.render('read'); // read.ejs 템플릿을 렌더링
 });
 
 module.exports = router;
